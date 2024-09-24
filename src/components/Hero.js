b19f689e4f6fa2056265061f3460b899993130ae
soke14 <joshsoke@gmail.com>
2024-09-23 22:59:26 -0600

    added spinner to home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, forwardRef } from 'react';
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Spinner, Center } from "@chakra-ui/react";
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 import Carousel from "./Carroussel";
 import { v4 as uuidv4 } from "uuid";
@@ -9,10 +9,12 @@ import { Link } from "react-router-dom";
 
 const Hero = forwardRef((props, ref) => {
   const [cards, setCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const loadImages = async () => {
       try {
+        setIsLoading(true);
         const data = await queryNotion(uuidv4(), true);
         const allImages = data.image_data;
 
@@ -28,6 +30,8 @@ const Hero = forwardRef((props, ref) => {
         setCards(newCards);
       } catch (error) {
         console.error('Error loading images:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -40,32 +44,47 @@ const Hero = forwardRef((props, ref) => {
       py="20" 
       ref={ref} 
       position="relative"
+      height="700px"
     >
-      <Carousel
-        cards={cards}
-        height="700px"
-        width="100%"
-        margin="0 auto"
-        offset={2}
-        showArrows={false}
-      />
+      {isLoading ? (
+        <Center height="100%">
+          <Spinner 
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="teal.500"
+            size="xl"
+          />
+        </Center>
+      ) : (
+        <>
+          <Carousel
+            cards={cards}
+            height="700px"
+            width="100%"
+            margin="0 auto"
+            offset={2}
+            showArrows={false}
+          />
 
-      <Link to="/work">
-        <Text
-          as="span"
-          fontSize="lg"
-          color="white"
-          position="absolute"
-          right="4rem"
-          bottom="3rem"
-          display="flex"
-          alignItems="center"
-          _hover={{ color: "teal.400", textDecoration: "underline" }}
-          zIndex="999"  // Ensure this is above the carousel but below the menu
-        >
-          View More <ArrowForwardIcon ml={1} />
-        </Text>
-      </Link>
+          <Link to="/work">
+            <Text
+              as="span"
+              fontSize="lg"
+              color="white"
+              position="absolute"
+              right="4rem"
+              bottom="3rem"
+              display="flex"
+              alignItems="center"
+              _hover={{ color: "teal.400", textDecoration: "underline" }}
+              zIndex="999"
+            >
+              View More <ArrowForwardIcon ml={1} />
+            </Text>
+          </Link>
+        </>
+      )}
     </Box>
   );
 });
